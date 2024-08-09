b7db8c57134d3afed3f8a1ceae3133d772416b98
Syahrul Romadhon <rmdhn.syahrul@gmail.com>
2024-08-09 22:31:56 +0700

    fix: semantic release

--- release.config.mjs
+++ release.config.mjs
@@ -43,13 +43,14 @@ export default {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     // "@semantic-release/npm",
-    // [
-    //   "@semantic-release/git",
-    //   {
-    //     "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
-    //     "message": "release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-    //   }
-    // ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
+        message:
+          "release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
     // "@semantic-release/github"
   ],
 };
