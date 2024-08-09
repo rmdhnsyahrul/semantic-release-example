1e637c321015eb96b3fce91db2da36ad7b349a0a
Syahrul Romadhon <rmdhn.syahrul@gmail.com>
2024-08-09 23:40:12 +0700

    feat: extend semantic release plugins 1

--- release.config.mjs
+++ release.config.mjs
@@ -3,53 +3,53 @@
  */
 export default {
   branches: ["main"],
-  // extends: "semantic-release-npm-github-publish",
-  plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        releaseRules: [
-          {
-            type: "build",
-            release: "patch",
-          },
-          {
-            type: "ci",
-            release: "patch",
-          },
-          {
-            type: "chore",
-            release: "patch",
-          },
-          {
-            type: "docs",
-            release: "patch",
-          },
-          {
-            type: "refactor",
-            release: "patch",
-          },
-          {
-            type: "style",
-            release: "patch",
-          },
-          {
-            type: "test",
-            release: "patch",
-          },
-        ],
-      },
-    ],
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    // "@semantic-release/npm",
-    [
-      "@semantic-release/git",
-      {
-        message:
-          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-      },
-    ],
-    "@semantic-release/github",
-  ],
+  extends: "semantic-release-npm-github-publish",
+  // plugins: [
+  //   [
+  //     "@semantic-release/commit-analyzer",
+  //     {
+  //       releaseRules: [
+  //         {
+  //           type: "build",
+  //           release: "patch",
+  //         },
+  //         {
+  //           type: "ci",
+  //           release: "patch",
+  //         },
+  //         {
+  //           type: "chore",
+  //           release: "patch",
+  //         },
+  //         {
+  //           type: "docs",
+  //           release: "patch",
+  //         },
+  //         {
+  //           type: "refactor",
+  //           release: "patch",
+  //         },
+  //         {
+  //           type: "style",
+  //           release: "patch",
+  //         },
+  //         {
+  //           type: "test",
+  //           release: "patch",
+  //         },
+  //       ],
+  //     },
+  //   ],
+  //   "@semantic-release/release-notes-generator",
+  //   "@semantic-release/changelog",
+  //   // "@semantic-release/npm",
+  //   [
+  //     "@semantic-release/git",
+  //     {
+  //       message:
+  //         "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+  //     },
+  //   ],
+  //   "@semantic-release/github",
+  // ],
 };
