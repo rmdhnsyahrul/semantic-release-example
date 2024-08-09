337f65c035188886f1274b84c6111b0fe3431b3b
Syahrul Romadhon <rmdhn.syahrul@gmail.com>
2024-08-09 23:33:58 +0700

    ci: add github plugins

--- release.config.mjs
+++ release.config.mjs
@@ -46,11 +46,10 @@ export default {
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "package-lock.json", "CHANGELOG.md"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
-    // "@semantic-release/github"
+    "@semantic-release/github",
   ],
 };
